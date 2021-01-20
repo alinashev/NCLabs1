bc899d5d8159ef1105aa028336db06cc31d0baa9
Alina <al.t.shevchenko@gmail.com>
2021-01-20 21:15:07 +0200

    #10: removed unused imports

--- src/main/java/com/company/controller/action/mainMenuAction/Exit.java
+++ src/main/java/com/company/controller/action/mainMenuAction/Exit.java
@@ -2,10 +2,8 @@ package com.company.controller.action.mainMenuAction;
 
 import com.company.controller.MenuController;
 import com.company.controller.action.Action;
-import com.company.view.View;
 
 public class Exit implements Action {
-
     @Override
     public void execute(MenuController controller) {
         controller.exitApp();
